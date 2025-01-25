d61ae5fac6719b12479b271fc2550cb97b908856
Mamli05 <maliavlar@gmail.com>
2025-01-25 17:02:44 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write the 5 numbers:");
+        int count=1;
+        int sum = 0;
+        int number = 0;
+        do{
+            System.out.println("Enter number #" + count + ": ");
+            String a = scanner.nextLine();
+            try{
+                number= Integer.parseInt(a);
+                count++;
+                sum+=number;
+            }catch (NumberFormatException badUserdata){
+                System.out.println("Invalid Value");
+
+            }
+
+        }while(count<=5);
+        System.out.println("Sum of 5 numbers is = " + sum);
+    }
+}
